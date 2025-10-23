0ae1dbdab9d4b48247b72416603f44ddc57b8627
Lionel Karlen <lionel.karlen@protonmail.com>
2025-10-24 01:04:06 +0200

    feat: add ark-ui and pandacss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
+import path from "node:path";
 
 export default defineConfig({
+  // https://stackoverflow.com/a/77249075
+  resolve: {
+    alias: {
+      "@style": path.resolve(__dirname, "./styled-system"),
+    }
+  },
   plugins: [devtools(), solidPlugin()],
   server: {
     port: 3000,

25991f843d98b187619e238bdcb9428e29432809
Lionel Karlen <lionel.karlen@protonmail.com>
2025-10-23 21:06:06 +0200

    chore: add flake.nix and devshells

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Solidjs + Pocketbase flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                typescript-language-server
+                pocketbase
+              ];
+            };
+
+          serve =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                pocketbase
+                mprocs
+              ];
+              shellHook = ''mprocs "deno task dev" "pocketbase serve"'';
+            };
+        };
+      }
+    );
+}
